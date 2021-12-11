ca04a83a0949f3703937b51af9f678f670acd3bd
Renato Ruppel <heruppel2@gmail.com>
2021-12-11 09:57:01 -0300

    config prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  endOfLine: 'crlf',
+};
