a972f9e1f63d7d87096f768b92259195851325dd
Renato Ruppel <heruppel2@gmail.com>
2021-12-11 10:10:21 -0300

    added post model and relation with user

--- script.ts
+++ script.ts
@@ -4,7 +4,14 @@ const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
-  const result = await prisma.user.findMany();
+  const result = await prisma.user.update({
+    data: {
+      name: 'Ronaldo Stopa ED'
+    },
+    where: {
+      id: 5
+    }
+  });
 
   console.log(result);
 }
